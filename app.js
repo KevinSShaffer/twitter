b97242ed1de745cd1f519aabcd8f0f34856e5089
Kevin Shaffer <kevinsshaffer@gmail.com>
2017-08-27 14:47:51 -0600

    completion of scaffolding, add routing, and homepage includes links

--- app.js
+++ app.js
@@ -1,60 +1,33 @@
-
 var express = require('express');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var passport = require('passport');
 var LocalStrategy = require('passport-local');
 
+// Setup app
 var app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
+
+// Authentication
 app.use(passport.initialize());
 app.use(passport.session());
-
 var User = require('./models/user');
 passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
+// Routes
+app.use(require('./routes/index'));
+
+// Middleware
 app.use(function(req, res, next) {
 	res.locals.user = req.user;
 	next();
 });
 
-app.get('/', function(req, res) {
-	res.render('index');
-});
-
-app.get('/login', function(req, res) {
-	res.render('login');
-});
-app.post('/login', passport.authenticate('local', {
-		failureRedirect: '/login',
-		successRedirect: '/'
-	}));
-
-app.get('/register', function(req, res) {
-	res.render('register');
-});
-app.post('/register', function(req, res) {
-	var newUser = req.body.user;	
-	User.register({ username: newUser.username }, newUser.password, function(err, user) {
-		if (err) {
-			res.send('Unable to register');
-		} else {
-			User.authenticate(newUser.username, newUser.password, function(err, result) {
-				if (err) {
-					res.send('Unable to authentice user');
-				} else {
-					res.user = result;
-					res.redirect('/');
-				}
-			});
-		}
-	});
-});
-
+// Start server and connect to DB
 var port = process.env.PORT || 3000;
 app.listen(port, function() {
 	console.log('App has started on port: ' + port);
---
+++ routes/index.js
@@ -0,0 +1,41 @@
+var express = require('express');
+var router = express.Router()
+var passport = require('passport');
+var User = require('../models/user')
+
+router.get('/', function(req, res) {
+	res.render('index');
+});
+
+router.get('/login', function(req, res) {
+	res.render('login');
+});
+router.post('/login', passport.authenticate('local', {
+		failureRedirect: '/login',
+		successRedirect: '/'
+	}));
+
+router.get('/register', function(req, res) {
+	res.render('register');
+});
+router.post('/register', function(req, res) {
+	var newUser = req.body.user;	
+	User.register({ username: newUser.username }, newUser.password, function(err, user) {
+		if (err) {
+			res.send('Unable to register');
+		} else {
+			User.authenticate(newUser.username, newUser.password, function(err, result) {
+				if (err) {
+					res.send('Unable to authentice user');
+				} else {
+					res.user = result;
+					res.redirect('/');
+				}
+			});
+		}
+	});
+});
+
+// logout
+
+module.exports = router;
