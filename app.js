5a445ee3ea007223d99eee1b3f520c23068e8339
Kevin Shaffer <kevinsshaffer@gmail.com>
2017-08-27 09:53:57 -0600

    setup basic routing

---
+++ app.js
@@ -0,0 +1,13 @@
+
+var express = require('express');
+
+var app = express();
+
+app.get('/', function(req, res) {
+	res.send('Hello world');
+});
+
+var port = process.env.PORT || 3000;
+app.listen(port, function() {
+	console.log('App has started on port: ' + port);
+});
